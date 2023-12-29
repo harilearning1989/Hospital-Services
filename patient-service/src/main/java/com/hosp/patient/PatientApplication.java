bed35ba8c7b1c2f44daee4d990a7af308f1f408f
harilearning1989 <59171886+harilearning1989@users.noreply.github.com>
2023-12-29 22:21:17 +0530

    modified

--- patient-service/src/main/java/com/hosp/patient/PatientApplication.java
+++ patient-service/src/main/java/com/hosp/patient/PatientApplication.java
@@ -1,5 +1,6 @@
 package com.hosp.patient;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -13,10 +14,19 @@ import org.springframework.context.annotation.PropertySource;
 		"com.hosp.patient.*"
 })
 @PropertySource("classpath:common-service-${spring.profiles.active}.properties")
-public class PatientApplication {
+public class PatientApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PatientApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+	  /*View prescription details
+	  View medication from doctor
+	  View doctor list
+	  View blood bank status,Test Reports
+	  View operation history
+	  View admit history. like bed, ward icu etc*/
+	}
 }
