4ba07128f86db0164da578e23ff220cd7048ca3c
harilearning1989 <59171886+harilearning1989@users.noreply.github.com>
2024-01-06 22:42:54 +0530

    modified

---
+++ hospital-mgmt-ui/src/app/hospital/modules/patient/create/create.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CreateComponent } from '../../../components/patient/create/create.component';
+
+
+
+@NgModule({
+  declarations: [
+    CreateComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CreateModule { }
